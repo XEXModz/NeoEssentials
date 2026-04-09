0bc247760dfa07d69cc8b1a9a4e5423fd1e3e04e
XEXModz <tsbarnes07@gmail.com>
2026-04-09 16:16:57 -0500

    Security: Replace CORS * with CorsHandler
    Replaced hardcoded CORS * with CorsHandler.apply().

--- src/main/java/com/zerog/neoessentials/resourcepacks/ResourcePackHandler.java
+++ src/main/java/com/zerog/neoessentials/resourcepacks/ResourcePackHandler.java
@@ -416,7 +416,7 @@ public class ResourcePackHandler implements HttpHandler {
         byte[] bytes = response.getBytes(StandardCharsets.UTF_8);
         
         exchange.getResponseHeaders().set("Content-Type", "application/json");
-        exchange.getResponseHeaders().set("Access-Control-Allow-Origin", "*");
+        com.zerog.neoessentials.webdashboard.security.CorsHandler.apply(exchange);
         exchange.sendResponseHeaders(statusCode, bytes.length);
         
         try (OutputStream os = exchange.getResponseBody()) {
